729d3c4d8601073025de516e314d565a7e5bef00
lizhen325 <zhenli336@gmail.com>
2016-03-04 15:30:47 +1300

    Add CartInfo table in database 	new file:   Model/CartInfo.cs 	modified:   Model/Model.csproj

---
+++ Model/CartInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class CartInfo
+    {
+        //Id    int
+        //CustomerId    int
+        //BookId        int
+        //BookCount     int
+        //BookUnitPrice decimal
+
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private int _customerId;
+
+        public int CustomerId
+        {
+            get { return _customerId; }
+            set { _customerId = value; }
+        }
+        private int _bookId;
+
+        public int BookId
+        {
+            get { return _bookId; }
+            set { _bookId = value; }
+        }
+        private int _bookCount;
+
+        public int BookCount
+        {
+            get { return _bookCount; }
+            set { _bookCount = value; }
+        }
+        private decimal _bookUnitPrice;
+
+        public decimal BookUnitPrice
+        {
+            get { return _bookUnitPrice; }
+            set { _bookUnitPrice = value; }
+        }
+    }
+}
