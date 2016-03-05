8efe6de4f52010a3cae0df792f5fd4ff801d25ae
lizhen325 <zhenli336@gmail.com>
2016-03-05 13:11:33 +1300

    completed sending email 	new file:   UI/Common/SendMail.cs 	modified:   UI/UI.csproj

---
+++ UI/Common/SendMail.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+namespace UI.Common
+{
+    public class SendMail
+    {
+        public bool Send(string MessageFrom, string MessageTo, string MessageSubject, string MessageBody)
+        {
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
+            client.EnableSsl = true;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("EmailAddress", "EmailPassword");
+            MailMessage msg = new MailMessage();
+            msg.To.Add(MessageTo);
+            msg.From = new MailAddress(MessageFrom);
+            msg.Subject = MessageSubject;
+            msg.Body = MessageBody;
+            msg.IsBodyHtml = true;
+            try
+            {
+                client.Send(msg);
+
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
